ba2df87240666542ff2f0e34987238285eacaf28
Teja Battula <kbattula059@gmail.com>
2026-08-24 21:28:13 +0530

    fix cors for persistent login

--- edutrack-frontend/src/App.jsx
+++ edutrack-frontend/src/App.jsx
@@ -65,7 +65,7 @@ function App() {
   
   }, []);
   if (authLoading) {
-    return <div>Checking login...</div>;
+    return <div style={{fontSize : "25px",margin:"auto"}}>Checking login...</div>;
   }
   if (!user) {
     return <LoginScreen setUser={setUser} />;
